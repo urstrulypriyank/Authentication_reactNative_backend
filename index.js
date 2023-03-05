a9a853a5e97ca69c71502091f6c2ab40aa62cc07
priyank rai <0priyankrai@gmail.com>
2023-03-05 14:26:40 +0530

    completeing login and signup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const port = process.env.PORT;
 // using database
 require("./db");
@@ -20,6 +21,7 @@ app.use(authRoutes);
 
 app.get("/", requireToken, (req, res) => {
   console.log(req.user);
+  ;
   res.send("Hello World");
 });
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("user");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { reset } = require("nodemon");
 
-router.post("/signUp", (req, res) => {
+router.post("/signup", (req, res) => {
   try {
     // cornsole.log("data received" + req.body);
     const { name, email, password, dob } = req.body;
@@ -14,7 +15,8 @@ router.post("/signUp", (req, res) => {
         .status(422)
         .send({ error: "Please fill all the fields correctly" });
     User.findOne({ email: email }).then(async (savedUser) => {
-      if (savedUser) return res.status(422).send({ error: "Invalid credentials" });
+      if (savedUser)
+        return res.status(422).send({ error: "Invalid credentials" });
       const user = new User({
         name,
         email,
@@ -39,8 +41,27 @@ router.post("/signUp", (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).send({ error: "all fields are mandatory" });
+  }
+  console.log(email + " " + password);
+  const savedUser = await User.findOne({ email: email });
+  //  If the user doesnot exist
+  if (!savedUser) return res.status(400).send({ error: "Invalid credential" });
 
-
-
+  try {
+    bcrypt.compare(password, savedUser.password, (err, result) => {
+      if (result) {
+        const token = jwt.sign({ _id: savedUser.id }, process.env.JWT_SECRET);
+        console.log("password matched");
+        return res.status(200).send({ token });
+      } else {
+        return res.status(400).send({ error: "Invalid credential" });
+      }
+    });
+  } catch (err) {}
+});
 
 module.exports = router;
